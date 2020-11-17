f99855a3a8ecf4a5a3a55b86b5cf41444fe24f29
BigT-coding <64039095+BigT-coding@users.noreply.github.com>
2020-11-17 11:36:09 -0800

    modify to block banned user

--- CSCI150_Project_Pages/sessionLogin.php
+++ CSCI150_Project_Pages/sessionLogin.php
@@ -11,11 +11,19 @@
         $sql = "SELECT * FROM userbase WHERE user_email ='$email'";// AND user_pwd='$hashedPass' LIMIT 1";
         $result = $conn->query($sql);
         $row = mysqli_fetch_assoc($result);
-
+		
         //uses fetch_assoc and if $row returns an array then continue because there is data
         if (is_array($row)){
+			//check for banned user; '1' is banned users
+			if($row['user_ban'] = '1'){
+				echo '<script type ="text/javascript">
+					alert("Your account is banned.");
+					window.location.href = "./pageLogin.php";
+					</script>';
+				return;
+			}
             //decrypts password hash and checks it according Bcrypt used in sessionSignup.php
-            if(password_verify($pass, $row['user_pwd'])){
+            else if(password_verify($pass, $row['user_pwd'])){
                 session_start();
                 $_SESSION['user_ID'] = $row['user_ID'];
                 $_SESSION['firstName'] = $row['user_first'];
@@ -28,7 +36,7 @@
                 // echo "Login Successful";
             }
             else {
-            header('Location: ./pageLogin.php?Login=FailedPassword');
+            //header('Location: ./pageLogin.php?Login=FailedPassword'); //this will bypass the echo javascript
             echo '<script type ="text/javascript">
                 alert("Email or Password incorrect!");
                 window.location.href = "./pageLogin.php";
@@ -36,12 +44,13 @@
             }
         }
         else {
-            header('Location: ./pageLogin.php?Login=FailedInfo');
+            //header('Location: ./pageLogin.php?Login=FailedInfo'); //this will bypass the echo javascript
             echo '<script type ="text/javascript">
                 alert("Email or Password incorrect!");
                 window.location.href = "./pageLogin.php";
                 </script>';
         }
+
         // close out connection to database
         $conn->close();
 
