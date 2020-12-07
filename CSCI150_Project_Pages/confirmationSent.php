3eb9c463a50979a43151858c8dc1408c35d373ac
Enrique Verduzco <user605@mail.fresnostate.edu>
2020-12-06 19:54:15 -0800

    Added Email Verification Functionality

---
+++ CSCI150_Project_Pages/confirmationSent.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./mainStyleSheet.css">
+</head>
+<body>
+    <div class="mainHolder">
+        <img class="logoLogin" src="./images/logoText.png" alt="Website Text Logo">
+        <div class="thanksText">
+            <h2>Thank you for registering! Please check your <a href="https://mail.google.com/mail/u/0/#inbox" title="email">email</a>
+                 for your verification link. </h2>
+        </div>
+        <a href="https://mail.google.com/mail/u/0/#inbox" >
+        <img class="emailLogo" src="./images/email.png" alt="Email Logo">
+        </a>
+    </div>
+</body>
--- CSCI150_Project_Pages/sessionSignup.php
+++ CSCI150_Project_Pages/sessionSignup.php
@@ -15,7 +15,7 @@
         $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
 
         // check if user email is already in database
-        $checkemail = "SELECT ID FROM userbase WHERE user_email ='$email' LIMIT 1";
+        $checkemail = "SELECT user_ID FROM userbase WHERE user_email ='$email' LIMIT 1";
         $result = $conn->query($checkemail);
         $count = mysqli_num_rows($result);
 
@@ -24,6 +24,22 @@
         // if statement to check any errors for testing purposes
         $sql = "INSERT INTO userbase (user_first, user_last, user_email, user_name, user_pwd)
         VALUES ('$first', '$last', '$email', '$username', '$hashedPass')";
+        $insert = $conn->query($sql);
+
+        //grabs user info to be able to user user_ID that was just generated
+        $userInfo = "SELECT user_ID FROM userbase WHERE user_email ='$email' LIMIT 1";// AND user_pwd='$hashedPass' LIMIT 1";
+        $userResult = $conn->query($userInfo);
+        $userRows = mysqli_fetch_assoc($userResult);
+
+        //SQL statements need prepared statement variables already created to function properly
+        $user_ID = $userRows['user_ID'];
+
+        // create user verfication key based on time of registration and user email
+        $regKey = password_hash(time() . $email, PASSWORD_DEFAULT);
+        $KeyInsert = "INSERT INTO register_base (register_key,user_ID)
+        VALUES ('$regKey','$user_ID')";
+        //$resultKey = $conn->query($KeyInsert);
+
 
         //if email is registered tell user that
         if ($count == 1) {
@@ -38,18 +54,59 @@
 
         // $conn->query($sql) completes insert query into database
         // if statement to check any errors for testing purposes
-        elseif ($conn->query($sql) === TRUE)
+        elseif ($conn->query($KeyInsert))
         {
-        //echo "New record created successfully";
-        //sends user back to LoginPage after registration is complete and data is stored in Azure
-        header('Location: ./pageLogin.php');
+            //
+            $body = "Click the link below to activate your account! <br><br><a href='https://donatr.live/verify.php?regKey=$regKey'>Activate my Donatr Account</a>";
+            $subject = "Donatr Account Activation";
+
+            $headers = array(
+                'Authorization: Bearer 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',
+                'Content-Type: application/json'
+            );
+
+            $data = array(
+                "personalizations" => array(
+                    array(
+                        "to" => array(
+                            array(
+                                "email" => "$email",
+                                "name" => $first
+                            )
+                        )
+                    )
+                ),
+                "from" => array(
+                    "email" => "admin@donatr.live"
+                ),
+                "subject" => $subject,
+                "content" => array(
+                    array(
+                        "type" => "text/html",
+                        "value" => $body
+                    )
+                )
+            );
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            echo "Please Check Email for Verification Code";
+            //sends user back to LoginPage after registration is complete and data is stored in Azure
+            header('Location: ./confirmationSent.php');
         }
+
         else {
         echo "Error";
         }
-
         // close out connection to database
         $conn->close();
-
     }
 ?>
